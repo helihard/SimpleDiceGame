88738e8b45d58a3c0dd2e5815e691da604fd63fe
Heli Hård Källoff <vierge.hard@gmail.com>
2024-09-05 15:51:30 +0200

    add method for game rounds

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class App {
 
+    // method for initialising simple dice game
     private static ArrayList<Player> initialize(Scanner scanner) {
         ArrayList<Player> players = new ArrayList<>();
         int numberOfPlayers, numberOfDice, numberOfSides;
@@ -27,12 +28,43 @@ public class App {
                 player.addDie(numberOfSides);
             }
         }
+        System.out.println("The game, Mrs Hudson, is on!");
         return players;
+    }
+
+    // method for game rounds
+    private static void takeTurn(ArrayList<Player> players, Scanner scanner) {
+        for (int i = 0; i < 2; i++) {
+                System.out.println("-------------------");
+                System.out.println("Spelomgång " + (i + 1) + "!");
+                System.out.println("-------------------");
+            for (Player player : players) {
+                System.out.println(player.getName() + ", gissa på ett värde:");
+                int guess = Integer.parseInt(scanner.nextLine().trim());
+                //System.out.println("current guess: " + guess);
+                player.rollDice();
+                int dieValue = player.getDieValue();
+                //System.out.println("current value: " + dieValue);
+                if (guess == dieValue) {
+                    player.increaseScore();
+                    System.out.println("Värdet av ditt slag är " + dieValue + ". Du gissade rätt!");
+                } else {
+                    System.out.println("Värdet av ditt slag är " + dieValue + ". Du gissade fel!");
+                }
+            }
+            System.out.println("Current score: ");
+            for (Player player : players) {
+                System.out.println(String.format("%s: %s points", player.getName(), player.getPoints()));
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         
-        initialize(scanner);
+        ArrayList<Player> players = new ArrayList<>();
+        players = initialize(scanner);
+        takeTurn(players, scanner);
+        scanner.close();
     }
 }
